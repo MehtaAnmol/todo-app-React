28d385081b942ac23b272f83885e2315733e6a17
AnmolMehta <anmolmehta1213@gmail.com>
2024-05-21 20:29:12 +0530

    Starting files Added

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import './App.css'
+
+function Input(){
+  
+}
+function Display(){
+
+}
+
+function App() {
+
+  return (
+    <div className = 'main__container'>
+      <h1>Todo App</h1>
+      <Input/>
+      <Display/>
+    </div>    
+  )
+}
+
+export default App
